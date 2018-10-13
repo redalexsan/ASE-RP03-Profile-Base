5de2ffacee34cadc0c5faac7aff1ac2423f87567
Unknown <alexsan1998@gmail.com>
2018-10-13 18:42:54 +0200

    Arreglos Layout

--- app/src/main/java/es/iessaladillo/pedrojoya/pr03/ui/main/MainActivity.java
+++ app/src/main/java/es/iessaladillo/pedrojoya/pr03/ui/main/MainActivity.java
@@ -3,17 +3,39 @@ package es.iessaladillo.pedrojoya.pr03.ui.main;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import es.iessaladillo.pedrojoya.pr03.R;
+import es.iessaladillo.pedrojoya.pr03.data.local.Database;
+import es.iessaladillo.pedrojoya.pr03.data.local.model.Avatar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView profileImage;
+    private TextView profileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // TODO
+        initViews();
+        defaultProfile();
+
+    }
+
+    private void defaultProfile() {
+        Avatar defaultProfile = Database.getInstance().getDefaultAvatar();
+        profileImage.setImageResource(defaultProfile.getImageResId());
+        profileName.setText(defaultProfile.getName());
+    }
+
+    private void initViews() {
+        profileImage = ActivityCompat.requireViewById(this, R.id.imageViewProfile);
+        profileName = ActivityCompat.requireViewById(this,R.id.textViewProfileName);
     }
 
     // DO NOT TOUCH
